fa4fd5e7d9ba4776d05c0ec4b379dcf73aed4099
Olexiy <olexiy.sokurenko@gmail.com>
2021-06-25 20:02:48 +0200

    Adding remote state in S3

--- main.tf
+++ main.tf
@@ -1,10 +1,16 @@
 # AWS VPC
 
-resource "aws_vpc" "main" {
+resource "aws_vpc" "main_vpc" {
+
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
 
   tags = {
-    Name = "main"
+    name = "${var.namespace}_main_vpc"
   }
+}
+
+output "vpc_arn" {
+  value       = aws_vpc.main_vpc.arn
+  description = "VPC identifier"
 }
--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "namespace" {
   description = "The project namespace to use for unique resource naming"
   type        = string
-  default = "test"
+  default     = "test"
 }
 
 variable "region" {
